a8c7fa9da3d2e8b85b910019e53cb040053332c0
Rıfat Hacıahmetoğlu <rfthao@outlook.com>
2023-09-17 19:41:24 +0300

    Ders 3 Sonu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:ProductComponent},//anasayfa componenti
+  {path:"products",component:ProductComponent},//url localhost:4200/products olursa
+  {path:"products/category/:categoryId",component:ProductComponent}//parametre vericeksek : koyup veriyoruz.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
