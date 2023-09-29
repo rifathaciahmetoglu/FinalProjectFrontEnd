34c0f7a57b8bb574960febe5248e37c4af175755
Rıfat Hacıahmetoğlu <rfthao@outlook.com>
2023-09-29 23:32:46 +0300

    Ders 5 Sonu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 
 const routes: Routes = [
   {path:"",component:ProductComponent},//anasayfa componenti
   {path:"products",component:ProductComponent},//url localhost:4200/products olursa
-  {path:"products/category/:categoryId",component:ProductComponent}//parametre vericeksek : koyup veriyoruz.
+  {path:"products/category/:categoryId",component:ProductComponent},//parametre vericeksek : koyup veriyoruz.
+  {path:"products/add",component:ProductAddComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,13 @@ import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 import { ToastrModule } from 'ngx-toastr';
 import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,8 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     NaviComponent,
     VatAddedPipe,
     FilterPipePipe,
-    CartSummaryComponent
+    CartSummaryComponent,
+    ProductAddComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +36,8 @@ import { CartSummaryComponent } from './components/cart-summary/cart-summary.com
     HttpClientModule,
     FormsModule,
     ToastrModule.forRoot({positionClass:"toast-bottom-right"}),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
