916d447a9e5d125242ee6e9c044c4e10f7faa955
Rıfat Hacıahmetoğlu <rfthao@outlook.com>
2023-09-30 15:25:04 +0300

    Kamp Sonu!!!!!!!!!!!!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {path:"",component:ProductComponent},//anasayfa componenti
   {path:"products",component:ProductComponent},//url localhost:4200/products olursa
   {path:"products/category/:categoryId",component:ProductComponent},//parametre vericeksek : koyup veriyoruz.
-  {path:"products/add",component:ProductAddComponent}
+  {path:"products/add",component:ProductAddComponent,canActivate:[LoginGuard]},
+  {path:"login",component:LoginComponent}
 ];
 
 @NgModule({
